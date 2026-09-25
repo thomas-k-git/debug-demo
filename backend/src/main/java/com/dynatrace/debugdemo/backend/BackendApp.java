ef1e4fd194cddfb8f1f459be7cad931916aa34c7
Thomas Klambauer <thomas.klambauer@dynatrace.com>
2026-09-25 13:57:04 +0200

    - make default example have same CPU business as well

--- backend/src/main/java/com/dynatrace/debugdemo/backend/BackendApp.java
+++ backend/src/main/java/com/dynatrace/debugdemo/backend/BackendApp.java
@@ -11,6 +11,8 @@ public class BackendApp {
 	public static void main(String[] args) throws Exception {
 		Server server = new Server(8081);
 
+		keepCpusBusy(2);
+
 		ServletContextHandler ctx = new ServletContextHandler();
 		ctx.setContextPath("/");
 		ctx.addServlet(BackendHandler.class, "/*");
@@ -20,4 +22,22 @@ public class BackendApp {
 		log.info("Backend listening on http://localhost:8081");
 		server.join();
 	}
+
+	private static void keepCpusBusy(int count) {
+		// heavy cpu load for all but one CPU
+		for (int i = 0; i < count; i++) {
+			var t = new Thread(() -> {
+				double sum = 1.6526;
+				while (true) {
+					sum *= 3;
+					if (sum == 1234.0f) {
+						System.out.println("unlikely black hole to prevent code elimination");
+					}
+				}
+			}, "doCalc-" + i);
+			t.setDaemon(true);
+			t.start();
+		}
+	}
+
 }
